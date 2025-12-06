dcf361be9d116e3dccb56db0fe88c1e1127b3999
Aditya Ray <adityaray7384@gmail.com>
2025-12-06 11:14:18 +0530

    first commit

---
+++ src/Todo.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+const Todo = () => {
+
+  const [newTodo, setNewTodo] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(newTodo){
+      setTodos([...todos, {text: newTodo, completed: false}])
+      setNewTodo('')
+    }
+  }
+
+  const handleDelete = (index) => {
+      const newTodos = [...todos]
+      newTodos[index].completed = !newTodos[index].completed;
+      setTodos(newTodos)
+  }
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} type="text" placeholder="Add new todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text}</span>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Todo
